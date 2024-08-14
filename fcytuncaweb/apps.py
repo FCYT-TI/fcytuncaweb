4547161d93c6560eeed469db67d715666c51f541
mhbaezc <mhbaezc@fctunca.edu.py>
2024-08-14 11:52:18 -0400

    Initial commit

---
+++ fcytuncaweb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FcytuncawebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fcytuncaweb'
---
+++ fcytuncaweb/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def raiz(request):
+    return redirect('/inicio')
+
+def resenha(request):
+    return render(request, 'resenhahistorica.html')
